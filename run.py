b179e6388596172f59f4139633ee458f7ba98428
TomH131 <twhill2010@hotmail.co.uk>
2024-06-19 08:42:45 +0000

    Adding in comments for each function

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 from random import randint
 
 def random_number():
+    """
+    This function will  return a random number between 1 and 100
+    """
     return randint(1, 100)
 
 def guess_number():
-    return int(input("Please guess a number between 1 and 100:"))
+    """
+    This will allow the user to input a number in order to make
+    their guess
+    """
+    return int(input("Please guess a number between 1 and 100:\n"))
     
 def check_answer(guess, number):
-
+    """
+    This will check the user's guess against the random number and
+    let the user know if they are correct or whether they need to 
+    guess a higher or lower number
+    """
     if guess == number():
         print("Congratulations! You guessed the correct number.")
         return True
@@ -18,6 +29,9 @@ def check_answer(guess, number):
     return False
 
 def start_game():
+    """
+    This function will start the game and also check for correct answer
+    """
     number = random_number()
     correct = False
     while not correct:
