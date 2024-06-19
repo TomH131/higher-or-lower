7e3305e8a32288feb940059263448d9ed1b4074d
TomH131 <twhill2010@hotmail.co.uk>
2024-06-19 08:37:49 +0000

    Adding check answer function

--- run.py
+++ run.py
@@ -6,5 +6,16 @@ def random_number():
 def guess_number():
     return int(input("Please guess a number between 1 and 100:"))
     
+def check_answer(guess, number):
+
+    if guess == number():
+        print("Congratulations! You guessed the correct number.")
+        return True
+    elif guess > number():
+        print("Unlucky! Please guess a lower number.")
+    else:
+        print("Unlucky! Please guess a higher number.")
+    return False
+
 
 
